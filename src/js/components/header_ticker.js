31bf4811fcef8006ef855f4d6df77123e91eb5d4
Samuel Beca <samuel.beca@gmail.com>
2019-07-23 15:29:55 +0200

    add ticker update on mouse move

--- src/js/components/header_ticker.js
+++ src/js/components/header_ticker.js
@@ -5,10 +5,25 @@ class HeaderTicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      tickerText: "test",
+      scrollY: 0,
       seconds: 0,
-      tickerText: "test"
+      mouseX: 0,
+      mouseY: 0
     };
   }
+
+  _onMouseMove(e) {
+    this.setState({
+      mouseX: e.nativeEvent.offsetX,
+      mouseY: e.nativeEvent.offsetY
+    });
+  }
+
+  setScroll = e => {
+    const { scrollTop } = this.state;
+    this.setState({ scrollY: window.scrollY });
+  };
 
   tick() {
     this.setState(prevState => ({
@@ -16,25 +31,26 @@ class HeaderTicker extends Component {
     }));
   }
 
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 10);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
   render() {
-    const { seconds } = this.state;
+    const { seconds, mouseX, mouseY, scrollY } = this.state;
     return (
-      <div className="header-ticker">
+      <div className="header-ticker" onMouseMove={this._onMouseMove.bind(this)}>
         <div className="header-ticker__inner">
           <Ticker direction="toRight" offset="run-in" speed={10}>
-            {index => <h1>{seconds}</h1>}
+            {index => <h1>{scrollY}</h1>}
           </Ticker>
         </div>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 10);
+    window.addEventListener("scroll", this.setScroll);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 }
 
