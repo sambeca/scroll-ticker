cd791c95a52721670bf7b8bd28a80556cba0d6d0
Samuel Beca <samuel.beca@gmail.com>
2019-07-23 11:46:05 +0200

    add react-ticker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Ticker from "react-ticker";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Ticker direction="toRight" offset="100%" speed={10}>
+          {index => <h1>Test</h1>}
+        </Ticker>
+      </div>
+    );
+  }
 }
 
 export default App;
